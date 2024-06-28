0675ab47916fab62d068ca6277334b9975ae14a9
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-06-28 22:45:18 +0200

    Create Wendy persona and 2 instruction prompt templates

---
+++ src/llm/prompt/configs/Instructions.ts
@@ -0,0 +1,11 @@
+import instructions from "../../../prompting/instructions.json";
+
+
+export class buildExampleConversationInstruction {
+  static readonly instruction: string = instructions["buildExampleConversationInstruction"]["instruction"];
+}
+
+
+export class converseWithPartnerAccordingToPersonaInstruction {
+  static readonly instruction: string = instructions["converseWithPartnerAccordingToPersonaInstruction"]["instruction"];
+}
---
+++ src/llm/prompt/configs/Personas.ts
@@ -0,0 +1,6 @@
+import personas from "../../../prompting/personas.json";
+
+export class Wendy  {
+  static readonly personaName: string = personas["Wendy"]["name"];
+  static readonly description: string = personas["Wendy"]["description"];
+}
