ca02e923adb86e0b298f5e28c7e2c4efbab7034e
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-06-28 17:46:50 +0300

    feat: linter and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,42 @@
-import { PrismaClient } from "@prisma/client";
-import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
+import { PrismaClient } from '@prisma/client';
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 import dotenv from 'dotenv';
 
-
 // Mount variables from .env into process.env
-dotenv.config()
-
+dotenv.config();
 
 // Small demo with Prisma: create a user in the database
 const prisma = new PrismaClient();
 
 async function main() {
-    // Create a user
-    const user = await prisma.user.create({
-        data: {
-            name: "John",
-        }
-    });
+  // Create a user
+  const user = await prisma.user.create({
+    data: {
+      name: 'John',
+    },
+  });
 
-    console.log("User created:", user);
+  console.log('User created:', user);
 }
 
 main()
-    .then(async () => await prisma.$disconnect())
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
+const bot = new Telegraf(process.env.BOT_TOKEN!);
 
-const bot = new Telegraf(process.env.BOT_TOKEN!)
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-bot.launch()
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
