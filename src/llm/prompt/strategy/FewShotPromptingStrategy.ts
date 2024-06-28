23fd1950e95fd16ab32b841e85c8bb83a46b9f32
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-06-28 22:45:36 +0200

    Update `FewShotPromptingStrategy` to use `PromptTemplate`

--- src/llm/prompt/strategy/FewShotPromptingStrategy.ts
+++ src/llm/prompt/strategy/FewShotPromptingStrategy.ts
@@ -1,23 +1,26 @@
 import { LlmProvider } from '../../providers/LlmProvider';
 import { Questionnaire } from '../../../model/data/Questionnaire';
 import { promptTemplates } from '../../../prompting/PromptTemplates';
-
-
-const template = promptTemplates.buildExampleConversationPrompt
-
-const secondTemplate = promptTemplates.coldConversationStartInstructionPrompt
+import { PromptTemplate, PromptTemplateVariables } from '../template/PromptTemplate';
+import { Wendy } from '../configs/Personas';
+import {
+  buildExampleConversationInstruction,
+  converseWithPartnerAccordingToPersonaInstruction,
+} from '../configs/Instructions';
 
 
 export class FewShotPromptingStrategy {
   async prompt(llmProvider: LlmProvider, questionnaire: Questionnaire): Promise<string> {
-    const craftedPrompt = template
-      .replace("{{Persona}}", "")
-      .replace("{{Task}}", ``)
-      .replace("{{Questionnaire}}", questionnaire.promptify())
-      .replace("{{OutputFormat}}", "Print the result in a SINGLE ```-backtick block.")
-      .replace("{{Name}}", questionnaire.dto.preferredName)
 
-    const response1 = await llmProvider.sendMessage(craftedPrompt)
+    const buildExampleConversationPrompt = new PromptTemplate(promptTemplates.buildExampleConversationPromptTemplate)
+      .set(PromptTemplateVariables.PERSONA_DESCRIPTION, Wendy.description)
+      .set(PromptTemplateVariables.INSTRUCTION, buildExampleConversationInstruction.instruction)
+      .set(PromptTemplateVariables.QUESTIONNAIRE, questionnaire.promptify())
+      .set(PromptTemplateVariables.OUTPUT_FORMAT, "Print the result in a SINGLE ```-backtick block.")
+      .set(PromptTemplateVariables.USER_NAME, questionnaire.dto.preferredName)
+      .build();
+
+    const response1 = await llmProvider.sendMessage(buildExampleConversationPrompt)
 
     const regex = /```([\s\S]*?)```/;
     const match = regex.exec(response1);
@@ -31,12 +34,15 @@ export class FewShotPromptingStrategy {
     console.log(`=============== response1 ===============\n${response1}`)
     console.log(`=============== conversationExample ===============\n${conversationExample}`)
 
-    const secondPrompt = secondTemplate
-      .replace("{{Persona}}", "Persona of helpful and loving online female pal named Wendy that helps her peer who may have a mental health issues and may currently be in pain or struggle.")
-      .replace("{{Task}}", ``)
-      .replace("{{Questionnaire}}", questionnaire.promptify())
-      .replace("{{ConversationExample}}", conversationExample)
-      .replace("{{Name}}", questionnaire.dto.preferredName);
+    const secondPrompt = new PromptTemplate(promptTemplates.coldConversationStartInstructionPromptTemplate)
+      .set(PromptTemplateVariables.PERSONA_DESCRIPTION, Wendy.description)
+      .set(PromptTemplateVariables.PERSONA_NAME, Wendy.personaName)
+      .set(PromptTemplateVariables.INSTRUCTION, converseWithPartnerAccordingToPersonaInstruction.instruction)
+      .set(PromptTemplateVariables.QUESTIONNAIRE, questionnaire.promptify())
+      .set(PromptTemplateVariables.CONVERSATION_EXAMPLE, conversationExample)
+      .set(PromptTemplateVariables.USER_NAME, questionnaire.dto.preferredName)
+      .set(PromptTemplateVariables.CHAT_MESSAGE, "Hello! Today I had a great day. I played football with my friends and hit 3 goals! Our team won over 2 goals!")
+      .build();
 
     const response2 = await llmProvider.sendMessage(secondPrompt);
 
