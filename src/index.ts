e8d047a52cab5a71573028650e1b5aab6ed5d6d3
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-06-28 02:08:26 +0200

    Add demo for the tg bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import dotenv from 'dotenv';
 
+
+// Mount variables from .env into process.env
+dotenv.config()
+
+
+// Small demo with Prisma: create a user in the database
 const prisma = new PrismaClient();
 
 async function main() {
@@ -10,7 +19,7 @@ async function main() {
         }
     });
 
-    console.log(user);
+    console.log("User created:", user);
 }
 
 main()
@@ -20,3 +29,17 @@ main()
         await prisma.$disconnect();
         process.exit(1);
     });
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN!)
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
