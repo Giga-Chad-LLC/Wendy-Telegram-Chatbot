426a7de08eec1bf62e444cc58b3b984df9875436
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-06-29 08:54:37 +0200

    Add cause argument to `ApplicationError`

--- src/app/errors/ApplicationError.ts
+++ src/app/errors/ApplicationError.ts
@@ -1,7 +1,10 @@
 
 export class ApplicationError extends Error {
-  constructor(message: string) {
+  public cause: Error | null;
+
+  constructor(message: string, cause: Error | null = null) {
     super(message);
     this.name = 'ApplicationError';
+    this.cause = cause;
   }
 }
