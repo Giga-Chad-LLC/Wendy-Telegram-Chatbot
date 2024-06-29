4120dfc797bf0dc73b40c4487f539ff7b45a4d84
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-06-29 11:22:38 +0200

    Implement utility test to check promptified `ChatMessage`

---
+++ tests/db.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { ChatMessage, ChatMessageRole } from '../src/db/models/ChatMessage';
+
+
+describe('Database models', () => {
+  it('ChatMessageRole correctly stringifies', async () => {
+    expect(ChatMessageRole.USER as string).toBe('user');
+    expect(ChatMessageRole.ASSISTANT as string).toBe('assistant');
+  });
+
+  it('ChatMessage correctly promptifies', async () => {
+    const chatMessage = new ChatMessage({
+      id: 1,
+      userId: 1,
+      dto: {
+        text: "Hello! I am Nick and I what to get to know you better!",
+        summary: "Nick wants to get to know you",
+        role: ChatMessageRole.USER,
+        sent: new Date(),
+        lastEdited: new Date(),
+      }
+    });
+
+    console.log(`Promptified message:\n"${chatMessage.promptify()}"`)
+    console.log()
+    console.log(`Promptified message summary:\n"${chatMessage.promptifyAsSummary()}"`)
+  });
+});
