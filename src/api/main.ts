05d0bed4f730917133dbeb711ea9b73635d584bb
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2024-06-30 15:26:26 +0300

    fix: cors

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { UserController } from './UserController';
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
